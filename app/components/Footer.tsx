f248d252788e82b2bd322df58f5e863cf7c35865
Vanderlei Oliveira <vanderlei327@gmail.com>
2025-11-28 00:03:26 -0300

    fix footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -11,13 +11,13 @@ import {
 
 function Footer() {
   return <footer className="bg-main_red w-full py-10 mt-10">
-    <div className="flex flex-col align-center w-11/12 mx-auto justify-center text-white md:flex-row">
+    <div className="flex flex-col justify-center w-11/12 m-auto justify-center text-white md:flex-row align-center md:items-center">
       <Image
         src="/images/logo-white.png"
         alt="Mais Tech Logo White"
         width={300}
         height={80}
-        className="w-[250px] pr-8 m-8 md:border-r-1 md:border-white"
+        className="w-[250px] m-auto mb-10 md:border-r-1 md:border-white md:pr-8 md:m-0 md:mr-8 md:h-17"
       />
 
       <div className="flex flex-col items-center gap-8 md:flex-row">
